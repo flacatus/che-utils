ea16c592445c81ca19b6611c3ea836d326e42863
flacatus <flacatus@redhat.com>
2020-02-18 12:13:32 +0100

    Add minikube install
    Signed-off-by: flacatus <flacatus@redhat.com>

---
+++ minikube.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if ! which minikube 
+then
+    printf "[ERROR] Minikube installation are required."
+    exit 1
+fi
+
+if [ -z "${1}" ]; then
+	MEMORY=8192
+else
+	MEMORY=${1}
+fi
+
+minikube start --memory=${MEMORY}
